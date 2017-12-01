3fb4495c8b2b4ff6c9b123cd7e65fa70840e8b25
Atom Yah <atom@yah>
2017-12-01 18:08:31 +0900

    first bot

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-print 'Atom';
+require_once __DIR__ .'/vendor/autoload.php';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
+foreach ($events as $event) {
+  replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
+  
+}
+
+
+function replyTextMessage($bot, $replyToken, $text) {
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+
+  if (!$response->isSucceeded()) {
+    error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }
+  
+}
 ?>
