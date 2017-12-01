dae1fc98028618edd42b708586fadead0d23c898
Atom Yah <atom@yah>
2017-12-01 18:13:37 +0900

    oumu gaeshi

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATUR
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
 foreach ($events as $event) {
-  replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
+  $bot->replyText($event->getReplyToken(), $event->getText());
   
 }
 
